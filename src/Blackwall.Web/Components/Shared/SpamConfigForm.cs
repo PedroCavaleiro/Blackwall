e33e16be6b581977391374bf8580413b58fd1947
Pedro Cavaleiro <p.cavaleiro@icloud.com>
2026-07-02 14:34:28 +0100

    Fixes #16

---
+++ src/Blackwall.Web/Components/Shared/SpamConfigForm.cs
@@ -0,0 +1,48 @@
+using Blackwall.Core.Entities;
+
+namespace Blackwall.Web.Components.Shared;
+
+public sealed class SpamConfigForm {
+    public int MaxMessagesPerWindow { get; set; } = 5;
+    public int RateLimitWindowSeconds { get; set; } = 5;
+    public int DuplicateMessageThreshold { get; set; } = 3;
+    public int DuplicateWindowSeconds { get; set; } = 5;
+    public bool DuplicateCrossChannelEnabled { get; set; } = true;
+    public int MentionLimit { get; set; } = 5;
+    public bool BlockInviteLinks { get; set; } = true;
+    public bool BlockSuspiciousLinks { get; set; }
+    public bool LinkWhitelistMode { get; set; }
+    public bool SafeBrowsingEnabled { get; set; }
+    public bool SafeBrowsingBlockUnsure { get; set; }
+    public bool IsEnabled { get; set; } = true;
+    public bool IsDryRun { get; set; }
+    public string LogChannelId { get; set; } = "";
+    public bool IsAntiRaidEnabled { get; set; }
+    public int AntiRaidJoinThreshold { get; set; } = 10;
+    public int AntiRaidWindowSeconds { get; set; } = 30;
+    public int AntiRaidCooldownMinutes { get; set; } = 30;
+    public bool IsAccountScoringEnabled { get; set; }
+    public bool AutoTimeoutMediumRiskOnJoin { get; set; }
+    public bool AutoTimeoutHighRiskOnJoin { get; set; }
+    public int AccountScoringTimeoutMinutes { get; set; } = 10;
+    public InfractionAction RateLimitAction { get; set; } = InfractionAction.Timeout;
+    public bool RateLimitAutoLockdown { get; set; }
+    public int RateLimitTimeoutMinutes { get; set; } = 10;
+    public int RateLimitMessageDeleteDays { get; set; }
+    public InfractionAction DuplicateAction { get; set; } = InfractionAction.Timeout;
+    public bool DuplicateAutoLockdown { get; set; }
+    public int DuplicateTimeoutMinutes { get; set; } = 10;
+    public int DuplicateMessageDeleteDays { get; set; }
+    public InfractionAction MentionLimitAction { get; set; } = InfractionAction.Timeout;
+    public bool MentionLimitAutoLockdown { get; set; }
+    public int MentionLimitTimeoutMinutes { get; set; } = 10;
+    public int MentionLimitMessageDeleteDays { get; set; }
+    public InfractionAction InviteLinkAction { get; set; } = InfractionAction.Timeout;
+    public bool InviteLinkAutoLockdown { get; set; }
+    public int InviteLinkTimeoutMinutes { get; set; } = 10;
+    public int InviteLinkMessageDeleteDays { get; set; }
+    public InfractionAction SuspiciousLinkAction { get; set; } = InfractionAction.Timeout;
+    public bool SuspiciousLinkAutoLockdown { get; set; }
+    public int SuspiciousLinkTimeoutMinutes { get; set; } = 10;
+    public int SuspiciousLinkMessageDeleteDays { get; set; }
+}
