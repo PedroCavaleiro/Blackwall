7567aa605762997508c191c0e191e73c8619e020
Pedro Cavaleiro <p.cavaleiro@icloud.com>
2026-07-02 10:23:31 +0100

    Adds missing migrations

---
+++ src/Blackwall.Infrastructure/Migrations/20260702084825_ModuleActionsPerModule.cs
@@ -0,0 +1,318 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blackwall.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModuleActionsPerModule : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "SuspiciousLinkAutoLockdown",
+                table: "SpamConfigurations",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SuspiciousLinkAction",
+                table: "SpamConfigurations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 1,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "RateLimitAutoLockdown",
+                table: "SpamConfigurations",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RateLimitAction",
+                table: "SpamConfigurations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 1,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "MentionLimitAutoLockdown",
+                table: "SpamConfigurations",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MentionLimitAction",
+                table: "SpamConfigurations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 1,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "InviteLinkAutoLockdown",
+                table: "SpamConfigurations",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "InviteLinkAction",
+                table: "SpamConfigurations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 1,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "DuplicateAutoLockdown",
+                table: "SpamConfigurations",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DuplicateAction",
+                table: "SpamConfigurations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 1,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DuplicateMessageDeleteDays",
+                table: "SpamConfigurations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DuplicateTimeoutMinutes",
+                table: "SpamConfigurations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 10);
+
+            migrationBuilder.AddColumn<int>(
+                name: "InviteLinkMessageDeleteDays",
+                table: "SpamConfigurations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "InviteLinkTimeoutMinutes",
+                table: "SpamConfigurations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 10);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MentionLimitMessageDeleteDays",
+                table: "SpamConfigurations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MentionLimitTimeoutMinutes",
+                table: "SpamConfigurations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 10);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RateLimitMessageDeleteDays",
+                table: "SpamConfigurations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RateLimitTimeoutMinutes",
+                table: "SpamConfigurations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 10);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SuspiciousLinkMessageDeleteDays",
+                table: "SpamConfigurations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SuspiciousLinkTimeoutMinutes",
+                table: "SpamConfigurations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 10);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DuplicateMessageDeleteDays",
+                table: "SpamConfigurations");
+
+            migrationBuilder.DropColumn(
+                name: "DuplicateTimeoutMinutes",
+                table: "SpamConfigurations");
+
+            migrationBuilder.DropColumn(
+                name: "InviteLinkMessageDeleteDays",
+                table: "SpamConfigurations");
+
+            migrationBuilder.DropColumn(
+                name: "InviteLinkTimeoutMinutes",
+                table: "SpamConfigurations");
+
+            migrationBuilder.DropColumn(
+                name: "MentionLimitMessageDeleteDays",
+                table: "SpamConfigurations");
+
+            migrationBuilder.DropColumn(
+                name: "MentionLimitTimeoutMinutes",
+                table: "SpamConfigurations");
+
+            migrationBuilder.DropColumn(
+                name: "RateLimitMessageDeleteDays",
+                table: "SpamConfigurations");
+
+            migrationBuilder.DropColumn(
+                name: "RateLimitTimeoutMinutes",
+                table: "SpamConfigurations");
+
+            migrationBuilder.DropColumn(
+                name: "SuspiciousLinkMessageDeleteDays",
+                table: "SpamConfigurations");
+
+            migrationBuilder.DropColumn(
+                name: "SuspiciousLinkTimeoutMinutes",
+                table: "SpamConfigurations");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "SuspiciousLinkAutoLockdown",
+                table: "SpamConfigurations",
+                type: "boolean",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldDefaultValue: false);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SuspiciousLinkAction",
+                table: "SpamConfigurations",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValue: 1);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "RateLimitAutoLockdown",
+                table: "SpamConfigurations",
+                type: "boolean",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldDefaultValue: false);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RateLimitAction",
+                table: "SpamConfigurations",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValue: 1);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "MentionLimitAutoLockdown",
+                table: "SpamConfigurations",
+                type: "boolean",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldDefaultValue: false);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MentionLimitAction",
+                table: "SpamConfigurations",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValue: 1);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "InviteLinkAutoLockdown",
+                table: "SpamConfigurations",
+                type: "boolean",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldDefaultValue: false);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "InviteLinkAction",
+                table: "SpamConfigurations",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValue: 1);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "DuplicateAutoLockdown",
+                table: "SpamConfigurations",
+                type: "boolean",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldDefaultValue: false);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DuplicateAction",
+                table: "SpamConfigurations",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValue: 1);
+        }
+    }
+}
---
+++ src/Blackwall.Infrastructure/Migrations/20260702085454_RemoveGlobalActionFields.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blackwall.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveGlobalActionFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Action",
+                table: "SpamConfigurations");
+
+            migrationBuilder.DropColumn(
+                name: "AutoLockdownEnabled",
+                table: "SpamConfigurations");
+
+            migrationBuilder.DropColumn(
+                name: "MessageDeleteDays",
+                table: "SpamConfigurations");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Action",
+                table: "SpamConfigurations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "AutoLockdownEnabled",
+                table: "SpamConfigurations",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MessageDeleteDays",
+                table: "SpamConfigurations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
